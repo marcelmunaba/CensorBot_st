1bb40e5b97d34dfa6de60d4a9db8c48e9fbbae1a
marcelmunaba <marcelmunaba@gmail.com>
2024-08-28 15:32:03 +0200

    migrated predict.py to streamlit_app.py
    early version

--- predict.py
+++ predict.py
@@ -13,23 +13,7 @@ def predict_curse(classifier, vectorizer, text, data):
     #print("Preprocessed text: ", new_text) #debugging
     new_text_transformed = vectorizer.transform([new_text])
     prediction = classifier.predict(new_text_transformed)
-    
-    if prediction == 1:
-        original_text = text
-        # Get the feature names (words) from the vectorizer
-        feature_names = vectorizer.get_feature_names_out()
-        # Iterate through each word in the preprocessed text
-        for word in new_text.split():
-            # Check if the word is not in the feature names (learned during training)
-            if word not in feature_names:
-                continue
-            # Replace the word with asterisks in the original text
-            original_text = original_text.replace(word, "*" * len(word))
-        
-        print("Censored text: ", original_text)
-        return 1, new_text
-    else:
-        return 0, new_text
+    return prediction, new_text
 
 def append_to_csv(file_path, new_word, severity_rating):
     with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
@@ -38,34 +22,36 @@ def append_to_csv(file_path, new_word, severity_rating):
         severity_rating_rounded = round(severity_rating, 1)
         writer.writerow([new_word, severity_rating_rounded])  # Enclose both elements within a list or tuple
 
+def censor_text(predict, text, vectorizer):
+    if predict == 1:
+        original_text = text
+        # Get the feature names (words) from the vectorizer
+        feature_names = vectorizer.get_feature_names_out()
+        # Iterate through each word in the preprocessed text
+        for word in text.split():
+            # Check if the word is not in the feature names (learned during training)
+            if word not in feature_names:
+                continue
+            # Replace the word with asterisks in the original text
+            original_text = original_text.replace(word, "*" * len(word))
+        
+        return original_text
+    else:
+        return text
+    
 if __name__ == "__main__":
-    #TODO: fetch from Database
-    data = pd.read_csv('./profanity_sample.csv', encoding='utf-8')
-    
-    # Train the model
-    classifier, vectorizer = train_model(data)
-    
-    print("Welcome to CensorBot :)")
-    while True:
-        # Test the model - Example : "What the heck is going on here?"
-        text_input = input("Hello! What do have in your mind? : ")
-        print("Testing the model with input: " + text_input)
-        
-        predict, preprocessedText = predict_curse(classifier, vectorizer, text_input, data)
-        if (predict) == 1:
-            print("Hey that's a bit rude! Watch your language >:|")
-        else :
-            print("I see. Congratulations on being polite :)")
-        
-        response = input("Do I get that right? (Y/n) ")
-        new_resp = response.lower()
-        if new_resp == 'n' and predict == 0:
-            #write the new vocabulary to the csv
-            append_to_csv('./profanity_sample.csv', preprocessedText, random.uniform(1.0, 2.0))
-            print("Okay. I'll keep that in mind :)\n")
-        elif new_resp == 'n' and predict == 1:
-            #write the new vocabulary to the csv
-            append_to_csv('./profanity_sample.csv', preprocessedText, random.uniform(0.0, 1.0))
-            print("Okay. I'll keep that in mind :)\n")
-        else:
-            print("Thanks for your feedback!\n")
+    #print("Welcome to CensorBot :)")
+    #while True:
+    #    
+    #    response = input("Do I get that right? (Y/n) ")
+    #    new_resp = response.lower()
+    #    if new_resp == 'n' and predict == 0:
+    #        #write the new vocabulary to the csv
+    #        append_to_csv('./profanity_sample.csv', preprocessedText, random.uniform(1.0, 2.0))
+    #        print("Okay. I'll keep that in mind :)\n")
+    #    elif new_resp == 'n' and predict == 1:
+    #        #write the new vocabulary to the csv
+    #        append_to_csv('./profanity_sample.csv', preprocessedText, random.uniform(0.0, 1.0))
+    #        print("Okay. I'll keep that in mind :)\n")
+    #    else:
+    #        print("Thanks for your feedback!\n")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,62 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import predict as pred
 
-#TODO: MIGRATE MINE FUNCTION DARI PREDICT.PY TO THIS FILE
-st.text("This is a test streamlit app")
-# Insert a chat message container.
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+# MODEL PREPARATION
+@st.cache_resource
+def load_model_and_vectorizer():
+    data = pd.read_csv("./profanity_sample.csv", encoding='utf-8')
+    classifier, vectorizer = pred.train_model(data)
+    return classifier, vectorizer, data
 
-# Display a chat input widget at the bottom of the app.
-st.chat_input("Say something")
+classifier, vectorizer, data = load_model_and_vectorizer()
+
+st.title("CensorBot")
+st.text("Welcome to CensorBot Demo!")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Insert a chat message container
+message = st.chat_message("assistant")
+message.write("Hello! What do you have in mind?")
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Predict using the model
+    predict, preprocessedText = pred.predict_curse(classifier, vectorizer, prompt, data)
+    st.write(f"Testing the model with input: {prompt}")
+    st.write(f"Preprocessed text: {preprocessedText}")
+    originalText = pred.censor_text(predict, preprocessedText, vectorizer)
+    st.write(f"Censored Text: {originalText}")
+
+    # Generate response based on prediction
+    if predict == 1:
+        response = "Hey that's a bit rude! Watch your language :angry:"
+    else:
+        response = "I see. Thank you for being polite :)"
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    # Recurring message
+    with st.chat_message("assistant"):
+        st.markdown("Hello! What do you have in mind?")
+    
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": "Hello! What do you have in mind?"})
