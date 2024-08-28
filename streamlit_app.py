c6d1ad483dd8d0f478838cb109e588e5ec0d1e0b
marcelmunaba <marcelmunaba@gmail.com>
2024-08-28 14:23:31 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
+#TODO: MIGRATE MINE FUNCTION DARI PREDICT.PY TO THIS FILE
 st.text("This is a test streamlit app")
+# Insert a chat message container.
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget at the bottom of the app.
+st.chat_input("Say something")
